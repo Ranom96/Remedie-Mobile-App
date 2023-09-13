5e030bfdd907f37166a2aa2c20c6c9efb5648170
santosalves.henrique <santosalves.henrique@eb.mil.br>
2023-09-13 15:23:22 -0300

    iniciando a tela de cadastre seus remédios

--- screens/Remedios.js
+++ screens/Remedios.js
@@ -1,12 +1,27 @@
 import * as React from 'react'
 import { SafeAreaView, Text } from 'react-native'
 import Header from '../components/Header'
+import DateInput from '../components/DateInput'
+import TextInputComponent from '../components/TextInputComponent'
 
-export default function Remedios () {
+
+
+export default function ConsultasCadastrar () {
+    const TextStyle = {
+        fontSize: 32,
+        textAlign: 'center',
+        fontWeight: '400',
+        marginHorizontal: 24,
+        paddingTop: 16
+
+    }
+
   return(
     <SafeAreaView>
       <Header title='Remédios'/>
-      <Text>Tela Remedios</Text>
+      <Text style={TextStyle}>Cadastre seus remédios</Text>
+        <TextInputComponent label="Medicamento" descricao='Insira o nome do remédio' placeholder='Nome'/>
+        <TextInputComponent label="Dosagem" descricao='Qual é a dosagem? Ex: 1 dose = 2 cápsulas' placeholder='Quantidade'/>
     </SafeAreaView>
   )
 }

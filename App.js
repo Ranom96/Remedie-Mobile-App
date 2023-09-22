3d4c29d5a2b3a6cdde3a605549d5e5de7122cf2c
Iev Mesquita <ievsouza622@gmail.com>
2023-09-21 23:48:19 -0300

    .

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import * as React from 'react'
-import { Text, SafeAreaView } from 'react-native';
-import NavBar from './components/NavBar'
+import * as React from "react";
+import { Text, SafeAreaView } from "react-native";
+import { useState, useEffect } from "react";
+import NavBar from "./components/NavBar";
+import SplashScreen from "./screens/Splash/SplashScreen";
 
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import react from "react";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <NavBar/>
-  );
-}
+  const [showSplash, setShowSplash] = useState(true);
 
+  useEffect(() => {
+    setTimeout(() => setShowSplash(false), 3000);
+  }, []);
+
+  return showSplash ? <SplashScreen /> : <NavBar />;
+}
--- components/NavBar.js
+++ components/NavBar.js
@@ -1,17 +1,21 @@
-import Home from '../screens/Home';
-import Remedios from '../screens/Remedios';
-import Consultas from '../screens/Consultas';
-import Exames from '../screens/Exames';
-import { View, Image, Text } from 'react-native';
+import Home from "../screens/Home";
+import Remedios from "../screens/Remedios";
+import Consultas from "../screens/Consultas";
+import Exames from "../screens/Exames";
+import { View, Image, Text } from "react-native";
 
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faHouseMedical, faNotesMedical, faPrescriptionBottleMedical, faStethoscope } from '@fortawesome/free-solid-svg-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import { Feather } from '@expo/vector-icons';
-import ConsultasCadastrar from '../screens/ConsultasCadastrar';
-import RemediosCadastrar from '../screens/RemediosCadastrar'
-
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  faHouseMedical,
+  faNotesMedical,
+  faPrescriptionBottleMedical,
+  faStethoscope,
+} from "@fortawesome/free-solid-svg-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { Feather } from "@expo/vector-icons";
+import ConsultasCadastrar from "../screens/ConsultasCadastrar";
+import RemediosCadastrar from "../screens/RemediosCadastrar";
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
@@ -21,27 +25,32 @@ export default function NavBar() {
       <Navigator
         screenOptions={{
           headerShown: false,
-          tabBarActiveTintColor: 'white',
-          tabBarInactiveTintColor: 'black',
+          tabBarActiveTintColor: "white",
+          tabBarInactiveTintColor: "black",
           tabBarStyle: {
-            backgroundColor: '#007AFF',
+            backgroundColor: "#007AFF",
           },
-          headerTintColor: 'white',
+          headerTintColor: "white",
           headerStyle: {
-            backgroundColor: '#007AFF',
+            backgroundColor: "#007AFF",
           },
           headerTitleStyle: {
             fontSize: 32,
           },
-          headerTitle: title="Remedie" ,
-          headerTitleAlign: 'center',
-        }}>
+          headerTitle: (title = "Remedie"),
+          headerTitleAlign: "center",
+        }}
+      >
         <Screen
           name="Home"
           component={Home}
           options={{
             tabBarIcon: ({ size, color }) => (
-              <FontAwesomeIcon icon={faHouseMedical} size={size} color={color} />
+              <FontAwesomeIcon
+                icon={faHouseMedical}
+                size={size}
+                color={color}
+              />
             ),
           }}
         />
@@ -50,7 +59,11 @@ export default function NavBar() {
           component={Remedios}
           options={{
             tabBarIcon: ({ size, color }) => (
-              <FontAwesomeIcon icon={faPrescriptionBottleMedical} size={size} color={color} />
+              <FontAwesomeIcon
+                icon={faPrescriptionBottleMedical}
+                size={size}
+                color={color}
+              />
             ),
           }}
         />
@@ -59,7 +72,11 @@ export default function NavBar() {
           component={Consultas}
           options={{
             tabBarIcon: ({ size, color }) => (
-              <FontAwesomeIcon icon={faNotesMedical} size={size} color={color} />
+              <FontAwesomeIcon
+                icon={faNotesMedical}
+                size={size}
+                color={color}
+              />
             ),
           }}
         />
